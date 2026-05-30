155bde56831f22735f14b5092049fc3d28e83e03
Juan Esteban Rivera <juanriverarestrepo0103@gmail.com>
2026-05-30 10:24:17 -0500

    feat(catalog): add dependency injection file in Application layer

---
+++ backend/services/catalog/src/Catalog.Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+using Catalog.Application.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalog.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.Scan(scan => scan
+            .FromAssembliesOf(typeof(DependencyInjection))
+            .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<>)), publicOnly: false)
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+            .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<,>)), publicOnly: false)
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+            .AddClasses(classes => classes.AssignableTo(typeof(IQueryable<>)), publicOnly: false)
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+        );
+
+        return services;
+    }
+}
